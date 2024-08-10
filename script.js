b596dfe1d2472e6040a7fdc38c0a9fe1debefb85
unknown <jaweriafareed123.bmj@gmail.com git config --global user.name JAWERIA-BALOCH>
2024-08-10 19:57:37 +0500

    table-row

---
+++ script.js
@@ -0,0 +1,10 @@
+// let num=prompt("Which table do you want (only 2 is available)");
+// for (let i = 1; i <= 10; i++) {
+//    document.write("<center>")
+//    // document.write("<margin-top=100px>")
+//    document.write("<div>")
+//    document.write(2 + "x" + i + "=" + i*2);
+//    document.write("</div>")
+//    // document.write("</margin-top=100px>")
+//    document.write("</center>")
+// }
